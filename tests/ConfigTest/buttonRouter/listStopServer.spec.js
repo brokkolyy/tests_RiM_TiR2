4bed18cf4d27e41e59f6e9f7398932575f7a0f6c
Глотова Елена Игоревна <glotova@rim.local>
2025-12-24 12:36:59 +0700

    Тесты параметра Остановить сервер, кнопка Роутер

---
+++ tests/ConfigTest/buttonRouter/listStopServer.spec.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const { test, expect } = require('@playwright/test');
+const { exec } = require('child_process');
+const ConfigPage = require(path.join(process.cwd(), 'pages', 'Configuration', 'ConfigPage.js'));
+
+test.describe('Навигация', () => {
+    test.beforeEach(async ({page}) => {
+        const config = new ConfigPage(page);
+        await config.goto();
+        await config.buttonRouterClick();
+    });
+
+    test('Нажать ЛКМ на “Остановить сервер“', async ({page}) => {
+        const stop = page.getByRole('menuitem', { name: 'Остановить сервер' });
+        await stop.click();
+
+        const err = page.getByRole('status', { name: 'Произошла ошибка' });
+        await expect(err).toBeVisible();
+    });
+
+    test('Повторно нажать ЛКМ на “Остановить сервер“', async ({page}) => {
+        const stop = page.getByRole('menuitem', { name: 'Остановить сервер' });
+        const run = page.getByRole('menuitem', { name: 'Запустить сервер', exact: true })
+        await run.click();
+        await stop.click();
+
+        const status = page.getByRole('status', { name: 'Сервер остановлен' });
+        await expect(status).toBeVisible();
+    });
+});
